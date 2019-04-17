3dc17cb465bc6f1638dac8c2e107e49671dc9700
wuqy <wuqy@fxft.net>
2019-04-17 17:29:08 +0800

    添加测试Controller

--- src/main/java/com/example/spdemo/SpdemoApplication.java
+++ src/main/java/com/example/spdemo/SpdemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.spdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class SpdemoApplication {
---
+++ src/main/java/com/example/spdemo/controller/TestControl.java
@@ -0,0 +1,15 @@
+package com.example.spdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestControl {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "111";
+    }
+}
